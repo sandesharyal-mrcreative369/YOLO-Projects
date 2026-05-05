02c4e24156056852b0bae045dd174e0eda0415a5
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-05 10:55:00 +0530

    Distinguise class names and apply the condition

--- car-counter-project/car-counter-project.py
+++ car-counter-project/car-counter-project.py
@@ -5,44 +5,51 @@ from ultralytics import YOLO
 import cvzone
 
 
-
 images = sorted(glob.glob("outputs/archive/train/images/*.jpg"))
 model= YOLO("yolov8n.pt")
 
 classNames = model.names
 masked_images = cv2.imread("mask.png")
 
-while True:
-    for img_path in images:
-        frame = cv2.imread(img_path)
-        frame = cv2.resize(frame, (640, 480))
-        masked_images = cv2.resize(masked_images, (640, 480))
-        mask_region = cv2.bitwise_and(frame, masked_images)
-        result = model(mask_region,stream=True)
+for img_path in images:
+    frame = cv2.imread(img_path)
+    frame = cv2.resize(frame, (640, 480))
+    masked_images = cv2.resize(masked_images, (640, 480))
+    mask_region = cv2.bitwise_and(frame, masked_images)
+    result = model(mask_region,stream=True)
 
 
-        for r in result:
-            boxes = r.boxes
-            print(boxes)
-            for box in boxes:
-                cls = int(box.cls[0])
-                x1,y1,x2,y2 = box.xyxy[0]
-                x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+    for r in result:
+        boxes = r.boxes
 
-                w,h = x2-x1,y2-y1
-                cvzone.cornerRect(frame,(x1,y1,w,h),l=10,t=1)
+        for box in boxes:
 
-                #confidence
-                confidence = math.ceil(box.conf[0]*100)/100
-                cvzone.putTextRect(frame,f"{confidence}%",(max(0,x1),max(0,y1-10)),1,1,offset=1)
-                cvzone.putTextRect(frame,f"{classNames[cls]}",(max(0,x1),max(0,y1-10)),scale=2,offset= 2,thickness=1)
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
 
-        cv2.imshow('Video', frame)
-        cv2.imshow('Mask', mask_region)
+            w,h = x2-x1,y2-y1
+            cvzone.cornerRect(frame,(x1,y1,w,h),l=10,t=1)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            exit()
+            #confidence
+            confidence = math.ceil(box.conf[0]*100)/100
+            #cvzone.putTextRect(frame,f"{confidence}%",(max(0,x1),max(0,y1-10)),1,1,offset=1)
+
+            #className
+            cls = int(box.cls[0])
+            class_classified = classNames[cls]
+
+            #Checking class condition
+            if class_classified == "car" or class_classified == "bus" \
+            or class_classified == "truck" and confidence>0.4:
+
+                cvzone.putTextRect(frame,f"{class_classified}",(max(0,x1),max(0,y1-10)),scale=2,offset= 2,thickness=1)
+
+    cv2.imshow('Video', frame)
+    cv2.imshow('Mask', mask_region)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        exit()
 
 
 
