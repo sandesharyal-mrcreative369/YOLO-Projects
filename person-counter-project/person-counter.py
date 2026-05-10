985dbe39534e1016d19be900bde547c5f604a707
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-10 16:33:36 +0530

    Adding video source file in our code

--- person-counter-project/person-counter.py
+++ person-counter-project/person-counter.py
@@ -6,19 +6,36 @@ import glob
 
 model = YOLO("yolov8n.pt")
 
-images = sorted(glob.glob("result/frames/frames/*.jpg"))
-print(images[:5])
-print(len(images))
+# images = sorted(glob.glob("result/frames/frames/*.jpg"))
+# print(images[:5])
+# print(len(images))
 
-for image in images:
-    pic = cv2.imread(image)
 
-    if  pic is None:
+
+# for image in images:
+#     pic = cv2.imread(image)
+
+
+video = cv2.VideoCapture("people.mp4")
+
+while True:
+    success , pic = video.read()
+
+    if  not success:
         continue
 
     pic = cv2.resize(pic, (640, 480))
-
     results = model(pic,stream=True)
+
+
+for r in results:
+    boxes = r.boxes
+
+    for box in boxes:
+        x1,y1,x2,y2 = box.xyxy[0]
+        x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+
+        cvzone.cornerRect(pic,(x1,y1,x2,y2))
 
     if cv2.waitKey(30) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
