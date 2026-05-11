ba92abba2148d226a33a86a6439851e773a761f9
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-11 21:25:22 +0530

    Adding some comments for our project

--- person-counter-project/person-counter.py
+++ person-counter-project/person-counter.py
@@ -44,7 +44,12 @@ while True:
         continue
 
     pic = cv2.resize(pic, (640, 480))
+
+    #Masked Images resizing
     masked_images = cv2.resize(masked_images, (640, 480))
+
+
+    #Applying Bitwise and between pic and masked_images for focused part.
     masked_region = cv2.bitwise_and(pic,masked_images)
 
     results = model(masked_region,stream=True)
@@ -65,7 +70,7 @@ while True:
             # Detection confidence
             confidence = math.ceil(box.conf[0] * 100) / 100
 
-            list_array = np.array([x1,y1,x2,y2,confidence])
+            list_array = np.array([x1,y1,x2,y2,confidence])  #Array for sort algorithm
             list_detection = np.vstack((list_detection,list_array))
 
     #Update tracker
@@ -87,24 +92,34 @@ while True:
             if counter_down.count(id) == 0:
                 counter_down.append(id)
 
-        cv2.putText(pic, f"Down:{len(counter_down)}", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        #Display counter on screen
+        cv2.putText(pic, f"Down:{len(counter_down)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         # For counting UP
         if line2[0] < cx < line2[2] and line2[1] - 50 < cy < line2[3] + 50:
 
+            #Reduces duplicate ID's
             if counter_up.count(id) == 0:
                 counter_up.append(id)
 
-        cv2.putText(pic, f"UP:{len(counter_up)}", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        #Display counter on screen
+        cv2.putText(pic, f"UP:{len(counter_up)}", (550, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     #Drawing Lines for counting
+
+    #Draw line for counting Down
     cv2.line(pic,(line1[0],line1[1]),(line1[2],line1[3]),color=(0,0,255),thickness=2)
+
+    #Draw line for counting up
     cv2.line(pic,(line2[0],line2[1]),(line2[2],line2[3]),color=(0,255,0),thickness=2)
 
+    #When q is press it exist
     if cv2.waitKey(30) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         exit()
 
+    #Display video on screen
     cv2.imshow("frame",pic)
 
+#Destroys the windows
 cv2.destroyAllWindows()
